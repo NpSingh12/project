75e2b7728534aa310a583afd8451d2105cc0a532
NpSingh12 <narinderpals404@gmail.com>
2024-05-03 18:03:14 +0530

    change some thing

--- src/app.js
+++ src/app.js
@@ -14,4 +14,4 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 app.use(express.static("Devil"));
-export default app;
+export { app };
